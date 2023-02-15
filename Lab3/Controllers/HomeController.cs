253e89ce8ce84cdb13ea5d7c5693f7ef23e9fd92
Reza Mrd <reza.mrd@gmail.com>
2023-02-15 16:52:53 -0500

    Lab 3

---
+++ Lab3/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() { return View(); }
+
+        public IActionResult SongForm() { return View(); }
+
+        [HttpPost]
+        public IActionResult Sing()
+        {
+
+            HttpContext.Session.SetString("bottleQunatties", Request.Form["bottleQunatties"]);
+            return View();
+
+        }
+
+        [HttpGet]
+        public IActionResult CreateStudent() { return View(); }
+
+        [HttpPost]
+        public IActionResult DisplayStudent(Student student)
+        {
+
+            return View(student);
+
+
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Lab3/Models/Student.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class Student
+    {
+        [Required]
+        [BindProperty(Name = "firstName")]
+        public string FName
+        {
+            get;
+            set;
+        }
+
+
+        [Required]
+        [BindProperty(Name = "lastName")]
+        public string LName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [EmailAddress]
+        [BindProperty(Name = "email")]
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [BindProperty(Name = "id")]
+        public string Id
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [BindProperty(Name = "description")]
+        public string Desc
+        {
+            get;
+            set;
+        }
+
+    }
+}
